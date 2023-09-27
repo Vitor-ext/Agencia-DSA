51867919a60278f5dcdaa747510625888e0b115d
VITOR SANTANA DE JESUS <sn1079379@cfp127aluno.local>
2023-09-27 11:59:09 -0300

    Pendente Transferencia

--- src/br/senai/sp/jandira/model/Cliente.java
+++ src/br/senai/sp/jandira/model/Cliente.java
@@ -1,11 +1,79 @@
 package br.senai.sp.jandira.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Cliente {
-    public String nome,  endereco;
-    public long cpf, rg, telefone;
+    private String nome,  endereco;
+    private long cpf, rg, telefone;
     Scanner scanner = new Scanner(System.in);
+
+    List<Cliente> listCliente = new ArrayList<>();
+
+    public Cliente pesquisarCliente(long cpf){
+        for (Cliente cliente : listCliente) {
+            long validaCpf = cliente.getCpf();
+            if (validaCpf == cpf){
+                return cliente;
+            }
+        }
+        return null;
+    }
+
+    public void listarClientes(){
+        for (Cliente cliente: listCliente) {
+            System.out.println(cliente.getNome());
+            System.out.println(cliente.getCpf());
+        }
+    }
+
+    public void adicionarCliente(Cliente cliente){
+        listCliente.add(cliente);
+    }
+
+
+
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+
+    public String getNome (){
+        return nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setRg(long rg) {
+        this.rg = rg;
+    }
+
+    public long getRg() {
+        return rg;
+    }
+
+    public void setTelefone(long telefone) {
+        this.telefone = telefone;
+    }
+
+    public long getTelefone() {
+        return telefone;
+    }
+
     public void cadastrarCliente(){
 
         System.out.println("/-/-/-/-/-/  Cadastro Clinte  /-/-/-/-/-/");
@@ -21,7 +89,6 @@ public class Cliente {
         telefone = scanner.nextLong();
         scanner.nextLine();
         System.out.println("/-/-/-/-/  Cadastro Finalizado /-/-/-/-/-/");
-
 
     }
 
